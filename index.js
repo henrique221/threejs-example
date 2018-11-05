a70ee7598da9db22a666d61f4dd844b085b17531
henriquesilva <henrique.silva@catho.com>
2018-11-05 12:50:34 -0200

    ok

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-const swig = require('swig');
 const router = express.Router();
 
 app.use(bodyParser.urlencoded({
